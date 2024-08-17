50a34d109654ef86b11a6b1e83808510f2f736da
Elwood Nelson <elwoodnelson@gmail.com>
2024-08-17 14:32:36 -0500

    other pages except Succes finished

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -1,29 +1,31 @@
 import { useDispatch } from "react-redux";
 import { useState } from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 
 function Comments () {
-    const [feelingRating, setFeelingRating] = useState('');
+    const [comments, setComments] = useState('');
     const dispatch = useDispatch();
+    const history = useHistory();
 
     const storeRank = () => {
         dispatch ({
-            type: "ADD_RANKING",
-            payload: {
-                feelingRank: feelingRating
-            }
+            type: "COMMENTS",
+            payload: comments
         })
+        history.push('/review')
     }
     return (
         <div>
-            <h2>How are you feeling today?</h2>
+            <h2>Any comments you want to leave?</h2>
 
-            <label>Feeling?</label>
-            <input 
+            <label>Comments</label>
+            <input className="comment"
                 data-testid="input"
-                type="number"
-                placeholder=""
-                value={feelingRating} />
+                type="text"
+                placeholder="enter comments here"
+                onChange={(e) => setComments(e.target.value)}
+                value={comments} />
             <button data-testid="next" onClick={storeRank}>Next</button>
 
         </div>
--- src/components/Understand/Understand.jsx
+++ src/components/Understand/Understand.jsx
@@ -1,29 +1,31 @@
 import { useDispatch } from "react-redux";
 import { useState } from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 
 function Understand () {
-    const [feelingRating, setFeelingRating] = useState('');
+    const [understandRating, setUnderstandRating] = useState('');
     const dispatch = useDispatch();
+    const history = useHistory();
 
     const storeRank = () => {
         dispatch ({
-            type: "ADD_RANKING",
-            payload: {
-                feelingRank: feelingRating
-            }
+            type: "UNDERSTAND_RANK",
+            payload: understandRating
         })
+        history.push('/supported')
     }
     return (
         <div>
-            <h2>How are you feeling today?</h2>
+            <h2>How well are you understanding the content?</h2>
 
             <label>Understand?</label>
             <input 
                 data-testid="input"
                 type="number"
-                placeholder=""
-                value={feelingRating} />
+                placeholder="(1-low, 5-high)"
+                onChange={(e) => setUnderstandRating(e.target.value)}
+                value={understandRating} />
             <button data-testid="next" onClick={storeRank}>Next</button>
 
         </div>
